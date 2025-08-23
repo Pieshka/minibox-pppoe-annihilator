62681137058f46cfc7c6d6ed1d5713c9b4cf0a6f
Pieszka <39170443+Pieshka@users.noreply.github.com>
2025-08-23 18:36:48 +0200

    Move u-boot files to another partition on arm64. Fix booting issues on NanoPi R2S

--- extern/board/minibox/arm64/post-image-nanopi-r2s.sh
+++ extern/board/minibox/arm64/post-image-nanopi-r2s.sh
@@ -4,12 +4,32 @@ set -eu
 # Configuration
 BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
-GENIMAGE_CFG="${BOARD_DIR}/genimage-nanopi-r2s.cfg"
+GENIMAGE_TEMPLATE="${BOARD_DIR}/genimage-nanopi-r2s.cfg.in"
+GENIMAGE_CFG="${BINARIES_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 ROOTPATH_TMP="$(mktemp -d)"
+KERNEL="${BINARIES_DIR}/Image"
+FDT="${BINARIES_DIR}/rk3328-nanopi-r2s.dtb"
+BOOTCMD_TEMPLATE="${BOARD_DIR}/boot-nanopi-r2s.cmd.in"
+BOOTCMD="${BINARIES_DIR}/boot.cmd"
+BOOTSCRIPT="${BINARIES_DIR}/boot.scr"
+
+# Prepare boot.cmd
+# for now we are just copying template
+cp "${BOOTCMD_TEMPLATE}" "${BOOTCMD}"
+
+# Generate boot.scr
+mkimage -A arm64 -T script -C none -n "Minibox" \
+    -d "${BOOTCMD}" "${BOOTSCRIPT}"
+
+# Prepare genimage.cfg
+BOOTFILES="${KERNEL} ${BOOTSCRIPT} ${FDT}"
+BOOTSIZE=$(du -cb ${BOOTFILES} | tail -n1 | cut -f1)
+BOOTSIZE=$(( (BOOTSIZE + 1024*1024 - 1) / (1024*1024) * 1024*1024 ))
+sed "s/@BOOT_SIZE@/${BOOTSIZE}/" "${GENIMAGE_TEMPLATE}" > "${GENIMAGE_CFG}"
 
 # Prepare trap
-trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
+trap 'rm -rf "${ROOTPATH_TMP}" "${GENIMAGE_CFG}" "${BOOTCMD}" "${BOOTSCRIPT}"' EXIT
 
 # Run genimage
 rm -rf "${GENIMAGE_TMP} ${ROOTPATH_TMP}"
