9ea9d3d3a094b2c227f9903f1381c232d7b9d2d1
Pieszka <39170443+Pieshka@users.noreply.github.com>
2025-08-30 20:31:09 +0200

    Update Limine version

--- extern/package/limine/host-limine.mk
+++ extern/package/limine/host-limine.mk
@@ -1,4 +1,4 @@
-HOST_LIMINE_VERSION = 9.5.3
+HOST_LIMINE_VERSION = 9.6.1
 HOST_LIMINE_SITE = https://github.com/limine-bootloader/limine/releases/download/v$(HOST_LIMINE_VERSION)
 HOST_LIMINE_SOURCE = limine-$(HOST_LIMINE_VERSION).tar.xz
 HOST_LIMINE_LICENSE = BSD-2-Clause
